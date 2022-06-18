dbdca9c1ef7a2a16cff6033275d78c53899e2a78
Nguyen Minh Ly <20521592@gm.uit.edu.vn>
2022-06-19 01:14:40 +0700

    update merge

---
+++ merge_predict/README.md
@@ -0,0 +1,9 @@
+# <center>Merge 2 file .txt từ 2 model khác nhau</center>
+
+Cách chúng tôi merge hiện tại là lấy hết nội dung từ 2 file. Chúng tôi sẽ xử lý phần overlap phía sau.
+
+Cách merge: 
+
+    python3 merge.py --txt_1 <fol_1> --txt_2 <fol_2>
+
+fol_1, fol_2: là path của 2 folder chứa file cần merge.
---
+++ merge_predict/merge.py
@@ -0,0 +1,44 @@
+from os import listdir
+from os.path import isfile, join
+import argparse
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--txt_1', type=str, required=True)
+    parser.add_argument('--txt_2', type=str, required=True)
+    return parser.parse_args()
+
+def list_all_file(mypath):
+    onlyfiles = [f  for f in listdir(mypath) if isfile(join(mypath, f))]
+    return onlyfiles
+
+def get_content_file(file_path):
+    try:
+        f1 = open(file_path, 'r')
+        lines = f1.readlines()
+        f1.close()
+    except: 
+        lines = []
+    return lines
+
+def merge(args):
+    path_1 = args.txt_1 if args.txt_1[-1] != '/' else (args.txt_1 + '/')
+    path_2 = args.txt_2 if args.txt_2[-1] == '/' else (args.txt_2 + '/')
+
+    # vi model yolo co the predict thieu file khi no khong detect dc chu trong anh. Nen ta lay tat ca file ma 2 model predict dc
+    folder1 = list_all_file(path_1)
+    folder2 = list_all_file(path_2)
+    all_txt = folder1 + list(set(folder2) - set(folder1))
+
+    for file in all_txt: 
+        lines_1 = get_content_file(path_1 + file)
+        lines_2 = get_content_file(path_2 + file)
+        merge = lines_1 + lines_2
+        f = open('./results/' +  file, 'w')
+        for j in merge: 
+            f.write(j)
+        f.close()
+        
+if __name__=="__main__":
+    args = get_parser()
+    merge(args)
